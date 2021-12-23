9f2b93cd51a0f2d4b66302aa0a9603d7765f0191
Jainam <jainam_s@icloud.comh>
2021-12-22 20:39:17 -0800

    Initial Commit

---
+++ iCoffee/iCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCoffeeApp.swift
+//  iCoffee
+//
+//  Created by Jainam  Shah  on 12/22/21.
+//
+
+import SwiftUI
+
+@main
+struct iCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
